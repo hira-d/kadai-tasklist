2e29ac2aaf9a79ea975417128ff839d6c2cbc66a
dai.hira <d.jacktennis@gmail.com>
2020-01-28 11:43:49 +0000

    tasklist-kadai15

--- db/migrate/20200118050313_create_tasks.rb
+++ db/migrate/20200118050313_create_tasks.rb
@@ -2,6 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :content
+      t.string :status
+      t.integer :user_id
 
       t.timestamps
     end
